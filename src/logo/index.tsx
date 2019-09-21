7e765473dd99a875f89522d46465c212641bcff9
dyte <dieter.walckiers@gmail.com>
2019-09-22 00:09:50 +0200

    1983 logo

---
+++ src/logo/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import "./logo.css";
+
+export interface ILogoProps {}
+
+export function Logo(props: ILogoProps) {
+    return (
+        <p>
+            <a href="/">1983</a>
+        </p>
+    );
+}
