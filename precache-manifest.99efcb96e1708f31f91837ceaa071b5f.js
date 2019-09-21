95b208769f275d79b2f42f77012485eb82822fb0
dyte <dieter.walckiers@gmail.com>
2019-09-21 23:13:53 +0200

    Updates

---
+++ precache-manifest.99efcb96e1708f31f91837ceaa071b5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab00b0e728d577f05b22d283876fa02a",
+    "url": "/1983/index.html"
+  },
+  {
+    "revision": "1dee18cf0b451f31ce8c",
+    "url": "/1983/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "62878776bf6daae81586",
+    "url": "/1983/static/js/2.d56e56e7.chunk.js"
+  },
+  {
+    "revision": "1dee18cf0b451f31ce8c",
+    "url": "/1983/static/js/main.91184a1b.chunk.js"
+  },
+  {
+    "revision": "be1d98cc1576f6ac2613",
+    "url": "/1983/static/js/runtime-main.fd7aabcf.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/1983/static/media/logo.25bf045c.svg"
+  }
+]);
